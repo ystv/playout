5146ca9dff1b0597be0ac6c93f684d9a970b9ed4
rmil <rhys.milling@googlemail.com>
2021-01-24 22:43:23 +0000

    Fixed test data to match latest schema

--- test-data.sql
+++ test-data.sql
@@ -1,51 +1,44 @@
 -- Create two generic channels of the two types
 INSERT INTO playout.channel
-(name, description, type, ingest_url, slate_url,
-playback_url, visibility, archive, dvr)
+(name, description, type, ingest_url, ingest_type, 
+slate_url, visibility, archive, dvr)
 VALUES
 ('The TV Channel', 'Top-end content', 'linear',
-'rtp://media-ingest.ystv.co.uk/sky1',
+'rtp://media-ingest.ystv.co.uk/sky1', 'rtp',
 'https://cdn.ystv.co.uk/channel-assets/holding.mp4',
-'https://media-serve.ystv.co.uk/sky1/manifest.m3u8',
 'public', true, true),
 ('MusicTV', 'A pop-up music channel!', 'event',
-'rtmp://media-ingest.ystv.co.uk/mtv',
+'rtmp://media-ingest.ystv.co.uk/mtv', 'rtmp',
 'https://cdn.ystv.co.uk/channel-assets/holding.mp4',
-'https://media-serve.ystv.co.uk/mtv/manifest.m3u8',
 'public', true, false);
 --
 -- Generate some programmes
 INSERT INTO playout.programmes
-(title, description, thumbnail)
+(title, description, thumbnail, type)
 VALUES
 ('Cooking Time!', 'We are in a Kitchen, so sandwiches',
-'https://cdn.ystv.co.uk/prog-assets/101.jpg'),
+'https://cdn.ystv.co.uk/prog-assets/101.jpg', 'live'),
 ('Bird Documentary', 'Big pigeons',
-'https://cdn.ystv.co.uk/prog-assets/103.jpg'),
-('Heavy Pop', 'Like heavy rock but pop',
-'https://cdn.ystv.co.uk/prog-assets/220.jpg'),
+'https://cdn.ystv.co.uk/prog-assets/103.jpg', 'vod'),
+('Heavy Pop', 'Like heavy rock but pop', 
+'https://cdn.ystv.co.uk/prog-assets/220.jpg', 'live'),
 ('Top Crimbo', 'The best christmas hits',
-'https://cdn.ystv.co.uk/prog-assets/560.jpg'),
+'https://cdn.ystv.co.uk/prog-assets/560.jpg', 'vod'),
 ('Funny comedy', 'epic funny comedy',
-'https://cdn.ystv.co.uk/prog-assets/348.jpg');
+'https://cdn.ystv.co.uk/prog-assets/348.jpg', 'vod');
 --
 -- Make a little schedule
-INSERT INTO playout.schedule
-(channel_id, programme_id, ingest_url,
-scheduled_start, scheduled_end, name)
+INSERT INTO playout.schedule_blocks
+(channel_id, programme_id, ingest_url, ingest_type,
+scheduled_start, scheduled_end)
 VALUES
-(1, 2, 'rtp://media-land.ystv.co.uk/vod/c_s01_e01.mp4',
-'2020-01-21 09:00:00.000', '2020-01-21 09:45:00.000',
-'Documentary: The pigeons'),
-(1, 5, 'rtp://media-land.ystv.co.uk/vod/com_s43_e02.mp4',
-'2020-01-21 09:45:00.000', '2020-01-21 10:00:00.000',
-'Comedy: Something very very funny!'),
-(1, 1, 'rtp://media-ingest.ystv.co.uk/ob2',
-'2020-01-21 10:30:00.000', '2020-01-21 12:00:00.000',
-'Cooking Time with Rhys!'),
-(2, 3, 'rtp://media-ingest.ystv.co.uk/ob1',
-'2020-01-21 09:00:00.000', '2020-01-21 11:00:00.000',
-'Hard Pop?'),
-(2, 4, 'rtp://media-land.ystv.co.uk/vod/crimbo.mp4',
-'2020-01-21 11:00:00.000', '2020-01-21 12:00:00.000',
-'Crimbo time');
+(1, 2, 'rtp://media-land.ystv.co.uk/player/e45t', 'rtp',
+'2020-01-21 09:00:00.000', '2020-01-21 09:45:00.000'),
+(1, 5, 'rtp://media-land.ystv.co.uk/player/ftge', 'rtp',
+'2020-01-21 09:45:00.000', '2020-01-21 10:00:00.000'),
+(1, 1, 'rtp://media-ingest.ystv.co.uk/ob2', 'rtp',
+'2020-01-21 10:30:00.000', '2020-01-21 12:00:00.000'),
+(2, 3, 'rtp://media-ingest.ystv.co.uk/ob1', 'rtp',
+'2020-01-21 09:00:00.000', '2020-01-21 11:00:00.000'),
+(2, 4, 'rtp://media-land.ystv.co.uk/player/j67u', 'rtp',
+'2020-01-21 11:00:00.000', '2020-01-21 12:00:00.000');
