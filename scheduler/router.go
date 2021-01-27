60ab3529bf88582c6964857ad331b646d65d1360
rmil <rhys.milling@googlemail.com>
2021-01-27 03:27:26 +0000

    New and update blocks by HTTP

--- scheduler/router.go
+++ scheduler/router.go
@@ -11,7 +11,8 @@ import (
 func (s *Scheduler) Router() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
-	r.HandleFunc("/new_block", s.newBlock).Methods("POST")
+	r.HandleFunc("/block", s.newBlock).Methods("POST")
+	r.HandleFunc("/block", s.updateBlock).Methods("PUT")
 	return r
 }
 
@@ -26,5 +27,34 @@ func (s *Scheduler) newBlock(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	s.NewBlock(r.Context(), block)
+	blockID, err := s.NewBlock(r.Context(), block)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	req := struct {
+		blockID int `json:"blockID"`
+	}{
+		blockID: blockID,
+	}
+
+	err = json.NewEncoder(w).Encode(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+}
+
+func (s *Scheduler) updateBlock(w http.ResponseWriter, r *http.Request) {
+	block := Block{}
+	err := json.NewDecoder(r.Body).Decode(&block)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = s.UpdateBlock(r.Context(), block)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
