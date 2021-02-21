f95e99fbde8f96fcaa8ed13b7f2945fe7fa1785b
rmil <rhys.milling@googlemail.com>
2021-02-21 01:44:42 +0000

    new brave

--- piper/brave/input.go
+++ piper/brave/input.go
@@ -1,18 +1,60 @@
 package brave
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
-
-	"github.com/ystv/playout/piper"
 )
 
-var _ piper.InputStore = &Brave{}
+type (
+	// NewInput information required to create a new Brave input
+	NewInput struct {
+		URI      string
+		Type     string
+		HasAudio bool
+		HasVideo bool
+		Volume   float64
+		Position int // defaut is auto. -1 for live
+		Width    int
+		Height   int
+	}
+	// NewInputResponse response from Brave
+	NewInputResponse struct {
+		InputID     int    `json:"id"`
+		UniversalID string `json:"uid"`
+	}
+)
 
 // New creates a new input
-func (b *Brave) New(ctx context.Context, i piper.NewInput) error {
-	return nil
+func (b *Brave) New(ctx context.Context, i NewInput) (NewInputResponse, error) {
+	reqBody, err := json.Marshal(i)
+	if err != nil {
+		return NewInputResponse{}, fmt.Errorf("failed to marshal new pipe json: %w", err)
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPut, b.endpoint+"/api/inputs",
+		bytes.NewReader(reqBody))
+	if err != nil {
+		return NewInputResponse{}, fmt.Errorf("failed to make request: %w", err)
+	}
+	httpRes, err := b.c.Do(req)
+	if err != nil {
+		return NewInputResponse{}, fmt.Errorf("failed to do request: %w", err)
+	}
+	defer httpRes.Body.Close()
+
+	body, err := io.ReadAll(httpRes.Body)
+	if err != nil {
+		return NewInputResponse{}, fmt.Errorf("failed to read response body: %w", err)
+	}
+	res := NewInputResponse{}
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return NewInputResponse{}, fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+	return res, nil
 }
 
 // Delete removes the input
